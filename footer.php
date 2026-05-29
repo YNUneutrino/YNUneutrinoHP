79fbdc4d124a249d836364e97f8de610089ced42
daigohirata <hirata-daigo-yc@ynu.jp>
2026-05-29 13:05:10 +0900

    Moify lab floor 5 -> 3

--- footer.php
+++ footer.php
@@ -5,12 +5,12 @@
   if ($lang === 'en') {
     ?>
       79-5 Tokiwadai, Hodogaya-ku, Yokohama 240-8501, Japan<br>
-      Yokohama National University, Energy Engineering Building, 5F<br>
+      Yokohama National University, Energy Engineering Building, 3F<br>
       Copyright (C) 2025 YNU neutrino group All Rights Reserved.
     <?php
   } else {
     ?>
-      〒240-8501 横浜市保土ヶ谷区常盤台79-5 横浜国立大学 エネルギー工学棟 5階<br>
+      〒240-8501 横浜市保土ヶ谷区常盤台79-5 横浜国立大学 エネルギー工学棟 3階<br>
       Copyright (C) 2025 YNU neutrino group All Rights Reserved.
     <?php
   }

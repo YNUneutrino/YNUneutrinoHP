a13363b039dec4d35187d7b8203a67b42ce7854a
daigohirata <hirata-daigo-yc@ynu.jp>
2026-05-29 13:17:03 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,18 +1,12 @@
-<div id="footer">
-  <?php
-  $lang = $lang ?? 'ja';
+<?php $lang = $lang ?? 'ja'; ?>
 
-  if ($lang === 'en') {
-    ?>
-      79-5 Tokiwadai, Hodogaya-ku, Yokohama 240-8501, Japan<br>
-      Yokohama National University, Energy Engineering Building, 3F<br>
-      Copyright (C) <?php echo date("Y"); ?> YNU neutrino group All Rights Reserved.
-    <?php
-  } else {
-    ?>
-      〒240-8501 横浜市保土ヶ谷区常盤台79-5 横浜国立大学 エネルギー工学棟 3階<br>
-      Copyright (C) <?php echo date("Y"); ?> YNU neutrino group All Rights Reserved.
-    <?php
-  }
-  ?>
+<div id="footer">
+  <?php if ($lang === 'en'): ?>
+    79-5 Tokiwadai, Hodogaya-ku, Yokohama 240-8501, Japan<br>
+    Yokohama National University, Energy Engineering Building, 3F<br>
+  <?php else: ?>
+    〒240-8501 横浜市保土ヶ谷区常盤台79-5 横浜国立大学 エネルギー工学棟 3階<br>
+  <?php endif; ?>
+  
+  Copyright (C) <?php echo date("Y"); ?> YNU neutrino group All Rights Reserved.
 </div>

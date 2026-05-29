65beb87235e99c5657dca5d191150e370cbd5f91
daigohirata <hirata-daigo-yc@ynu.jp>
2026-05-29 13:13:37 +0900

    Update copyright to 2026

--- footer.php
+++ footer.php
@@ -6,12 +6,12 @@
     ?>
       79-5 Tokiwadai, Hodogaya-ku, Yokohama 240-8501, Japan<br>
       Yokohama National University, Energy Engineering Building, 3F<br>
-      Copyright (C) 2025 YNU neutrino group All Rights Reserved.
+      Copyright (C) <?php echo date("Y"); ?> YNU neutrino group All Rights Reserved.
     <?php
   } else {
     ?>
       〒240-8501 横浜市保土ヶ谷区常盤台79-5 横浜国立大学 エネルギー工学棟 3階<br>
-      Copyright (C) 2025 YNU neutrino group All Rights Reserved.
+      Copyright (C) <?php echo date("Y"); ?> YNU neutrino group All Rights Reserved.
     <?php
   }
   ?>
